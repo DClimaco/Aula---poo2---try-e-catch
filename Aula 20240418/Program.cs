c6261664a6457337931be00f10e922d8d6b1a966
Daniel Carvalho Climaco <111577285+DClimaco@users.noreply.github.com>
2024-04-18 08:42:26 -0300

    Add files via upload

---
+++ Aula 20240418/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_20240418
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool tioPaulo = false;
+            int filhos = 3, heranca = 200000;
+
+            try
+            {                
+                if(tioPaulo)
+                {
+                    throw (new Exception());
+                }
+                Console.WriteLine("Herança pra cada: " + heranca / filhos);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ele ainda está vivo");                
+            }
+            finally
+            {
+                Console.WriteLine("Finalizando...");
+            }
+        }
+    }
+}
